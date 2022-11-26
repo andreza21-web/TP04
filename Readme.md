8e5dfdeddc18bd6656f588ffe6d26bcb9d035bf8
Andreza Pessoa <andreza.21pessoa@gmail.com>
2022-11-25 23:44:03 -0300

    criacao de um controller com testes unitario e swagger

---
+++ Readme.md
@@ -0,0 +1,17 @@
+
+# SWII6
+
+## Autores
+Andreza do Nascimento Pessoa Matricula: Cb3012191
+
+Lidiane Souza Soares Matricula: CB3012026
+
+
+## Rodar aplicação
+
+editor de swagger online - https://editor.swagger.io/
+
+### Teste de Integração
+
+Para executar os testes de integração, é preciso ter uma  instância do SQL Server em execução.
+
---
+++ WideWorldImporters.API.UnitTests/DbContextExtensions.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using WideWorldImporters.API.Models;
+
+namespace WideWorldImporters.API.UnitTests
+{
+    public static class DbContextExtensions
+    {
+        public static void Seed(this WideWorldImportersDbContext dbContext)
+        {
+            // Add entities for DbContext instance
+
+            dbContext.StockItems.Add(new StockItem
+            {
+                StockItemID = 1,
+                StockItemName = "string",
+                SupplierID = 0,
+                ColorID = 0,
+                UnitPackageID = 0,
+                OuterPackageID = 0,
+                Brand = "string",
+                Size = "string",
+                LeadTimeDays = 0,
+                QuantityPerOuter = 0,
+                IsChillerStock = false,
+                Barcode = "string",
+                TaxRate = 0,
+                UnitPrice = 0,
+                RecommendedRetailPrice = 0,
+                TypicalWeightPerUnit = 0,
+                MarketingComments = "string",
+                InternalComments = "string",
+                CustomFields = "string",
+                Tags = "string",
+                SearchDetails = "string",
+                LastEditedBy = 0,
+                ValidFrom = new DateTime(2016, 5, 31, 23, 11, 0),
+                ValidTo = new DateTime(9999, 12, 31, 23, 59, 59)
+            });
+
+            dbContext.StockItems.Add(new StockItem
+            {
+                StockItemID = 2,
+                StockItemName = "string",
+                SupplierID = 0,
+                ColorID = 0,
+                UnitPackageID = 0,
+                OuterPackageID = 0,
+                Brand = "string",
+                Size = "string",
+                LeadTimeDays = 0,
+                QuantityPerOuter = 0,
+                IsChillerStock = false,
+                Barcode = "string",
+                TaxRate = 0,
+                UnitPrice = 0,
+                RecommendedRetailPrice = 0,
+                TypicalWeightPerUnit = 0,
+                MarketingComments = "string",
+                InternalComments = "string",
+                CustomFields = "string",
+                Tags = "string",
+                SearchDetails = "string",
+                LastEditedBy = 0,
+                ValidFrom = new DateTime(2016, 5, 31, 23, 11, 0),
+                ValidTo = new DateTime(9999, 12, 31, 23, 59, 59)
+            });
+
+            dbContext.StockItems.Add(new StockItem
+            {
+                StockItemID = 3,
+                StockItemName = "string",
+                SupplierID = 0,
+                ColorID = 0,
+                UnitPackageID = 0,
+                OuterPackageID = 0,
+                Brand = "string",
+                Size = "string",
+                LeadTimeDays = 0,
+                QuantityPerOuter = 0,
+                IsChillerStock = false,
+                Barcode = "string",
+                TaxRate = 0,
+                UnitPrice = 0,
+                RecommendedRetailPrice = 0,
+                TypicalWeightPerUnit = 0,
+                MarketingComments = "string",
+                InternalComments = "string",
+                CustomFields = "string",
+                Tags = "string",
+                SearchDetails = "string",
+                LastEditedBy = 0,
+                ValidFrom = new DateTime(2016, 5, 31, 23, 11, 0),
+                ValidTo = new DateTime(9999, 12, 31, 23, 59, 59)
+            });
+
+            dbContext.StockItems.Add(new StockItem
+            {
+                StockItemID = 4,
+                StockItemName = "string",
+                SupplierID = 0,
+                ColorID = 0,
+                UnitPackageID = 0,
+                OuterPackageID = 0,
+                Brand = "string",
+                Size = "string",
+                LeadTimeDays = 0,
+                QuantityPerOuter = 0,
+                IsChillerStock = false,
+                Barcode = "string",
+                TaxRate = 0,
+                UnitPrice = 0,
+                RecommendedRetailPrice = 0,
+                TypicalWeightPerUnit = 0,
+                MarketingComments = "string",
+                InternalComments = "string",
+                CustomFields = "string",
+                Tags = "string",
+                SearchDetails = "string",
+                LastEditedBy = 0,
+                ValidFrom = new DateTime(2016, 5, 31, 23, 11, 0),
+                ValidTo = new DateTime(9999, 12, 31, 23, 59, 59)
+            });
+
+            dbContext.StockItems.Add(new StockItem
+            {
+                StockItemID = 5,
+                StockItemName = "string",
+                SupplierID = 0,
+                ColorID = 0,
+                UnitPackageID = 0,
+                OuterPackageID = 0,
+                Brand = "string",
+                Size = "string",
+                LeadTimeDays = 0,
+                QuantityPerOuter = 0,
+                IsChillerStock = false,
+                Barcode = "string",
+                TaxRate = 0,
+                UnitPrice = 0,
+                RecommendedRetailPrice = 0,
+                TypicalWeightPerUnit = 0,
+                MarketingComments = "string",
+                InternalComments = "string",
+                CustomFields = "string",
+                Tags = "string",
+                SearchDetails = "string",
+                LastEditedBy = 0,
+                ValidFrom = new DateTime(2016, 5, 31, 23, 11, 0),
+                ValidTo = new DateTime(9999, 12, 31, 23, 59, 59)
+            });
+
+            dbContext.StockItems.Add(new StockItem
+            {
+                StockItemID = 6,
+                StockItemName = "string",
+                SupplierID = 0,
+                ColorID = 0,
+                UnitPackageID = 0,
+                OuterPackageID = 0,
+                Brand = "string",
+                Size = "string",
+                LeadTimeDays = 0,
+                QuantityPerOuter = 0,
+                IsChillerStock = false,
+                Barcode = "string",
+                TaxRate = 0,
+                UnitPrice = 0,
+                RecommendedRetailPrice = 0,
+                TypicalWeightPerUnit = 0,
+                MarketingComments = "string",
+                InternalComments = "string",
+                CustomFields = "string",
+                Tags = "string",
+                SearchDetails = "string",
+                LastEditedBy = 0,
+                ValidFrom = new DateTime(2016, 5, 31, 23, 11, 0),
+                ValidTo = new DateTime(9999, 12, 31, 23, 59, 59)
+            });
+
+            dbContext.StockItems.Add(new StockItem
+            {
+                StockItemID = 7,
+                StockItemName = "string",
+                SupplierID = 0,
+                ColorID = 0,
+                UnitPackageID = 0,
+                OuterPackageID = 0,
+                Brand = "string",
+                Size = "string",
+                LeadTimeDays = 0,
+                QuantityPerOuter = 0,
+                IsChillerStock = false,
+                Barcode = "string",
+                TaxRate = 0,
+                UnitPrice = 0,
+                RecommendedRetailPrice = 0,
+                TypicalWeightPerUnit = 0,
+                MarketingComments = "string",
+                InternalComments = "string",
+                CustomFields = "string",
+                Tags = "string",
+                SearchDetails = "string",
+                LastEditedBy = 0,
+                ValidFrom = new DateTime(2016, 5, 31, 23, 11, 0),
+                ValidTo = new DateTime(9999, 12, 31, 23, 59, 59)
+            });
+
+            dbContext.StockItems.Add(new StockItem
+            {
+                StockItemID = 8,
+                StockItemName = "string",
+                SupplierID = 0,
+                ColorID = 0,
+                UnitPackageID = 0,
+                OuterPackageID = 0,
+                Brand = "string",
+                Size = "string",
+                LeadTimeDays = 0,
+                QuantityPerOuter = 0,
+                IsChillerStock = false,
+                Barcode = "string",
+                TaxRate = 0,
+                UnitPrice = 0,
+                RecommendedRetailPrice = 0,
+                TypicalWeightPerUnit = 0,
+                MarketingComments = "string",
+                InternalComments = "string",
+                CustomFields = "string",
+                Tags = "string",
+                SearchDetails = "string",
+                LastEditedBy = 0,
+                ValidFrom = new DateTime(2016, 5, 31, 23, 11, 0),
+                ValidTo = new DateTime(9999, 12, 31, 23, 59, 59)
+            });
+
+            dbContext.StockItems.Add(new StockItem
+            {
+                StockItemID = 9,
+                StockItemName = "string",
+                SupplierID = 0,
+                ColorID = 0,
+                UnitPackageID = 0,
+                OuterPackageID = 0,
+                Brand = "string",
+                Size = "string",
+                LeadTimeDays = 0,
+                QuantityPerOuter = 0,
+                IsChillerStock = false,
+                Barcode = "string",
+                TaxRate = 0,
+                UnitPrice = 0,
+                RecommendedRetailPrice = 0,
+                TypicalWeightPerUnit = 0,
+                MarketingComments = "string",
+                InternalComments = "string",
+                CustomFields = "string",
+                Tags = "string",
+                SearchDetails = "string",
+                LastEditedBy = 0,
+                ValidFrom = new DateTime(2016, 5, 31, 23, 11, 0),
+                ValidTo = new DateTime(9999, 12, 31, 23, 59, 59)
+            });
+
+            dbContext.StockItems.Add(new StockItem
+            {
+                StockItemID = 10,
+                StockItemName = "string",
+                SupplierID = 0,
+                ColorID = 0,
+                UnitPackageID = 0,
+                OuterPackageID = 0,
+                Brand = "string",
+                Size = "string",
+                LeadTimeDays = 0,
+                QuantityPerOuter = 0,
+                IsChillerStock = false,
+                Barcode = "string",
+                TaxRate = 0,
+                UnitPrice = 0,
+                RecommendedRetailPrice = 0,
+                TypicalWeightPerUnit = 0,
+                MarketingComments = "string",
+                InternalComments = "string",
+                CustomFields = "string",
+                Tags = "string",
+                SearchDetails = "string",
+                LastEditedBy = 0,
+                ValidFrom = new DateTime(2016, 5, 31, 23, 11, 0),
+                ValidTo = new DateTime(9999, 12, 31, 23, 59, 59)
+            });
+
+            dbContext.StockItems.Add(new StockItem
+            {
+                StockItemID = 11,
+                StockItemName = "string",
+                SupplierID = 0,
+                ColorID = 0,
+                UnitPackageID = 0,
+                OuterPackageID = 0,
+                Brand = "string",
+                Size = "string",
+                LeadTimeDays = 0,
+                QuantityPerOuter = 0,
+                IsChillerStock = false,
+                Barcode = "string",
+                TaxRate = 0,
+                UnitPrice = 0,
+                RecommendedRetailPrice = 0,
+                TypicalWeightPerUnit = 0,
+                MarketingComments = "string",
+                InternalComments = "string",
+                CustomFields = "string",
+                Tags = "string",
+                SearchDetails = "string",
+                LastEditedBy = 0,
+                ValidFrom = new DateTime(2016, 5, 31, 23, 11, 0),
+                ValidTo = new DateTime(9999, 12, 31, 23, 59, 59)
+            });
+
+            dbContext.StockItems.Add(new StockItem
+            {
+                StockItemID = 12,
+                StockItemName = "string",
+                SupplierID = 0,
+                ColorID = 0,
+                UnitPackageID = 0,
+                OuterPackageID = 0,
+                Brand = "string",
+                Size = "string",
+                LeadTimeDays = 0,
+                QuantityPerOuter = 0,
+                IsChillerStock = false,
+                Barcode = "string",
+                TaxRate = 0,
+                UnitPrice = 0,
+                RecommendedRetailPrice = 0,
+                TypicalWeightPerUnit = 0,
+                MarketingComments = "string",
+                InternalComments = "string",
+                CustomFields = "string",
+                Tags = "string",
+                SearchDetails = "string",
+                LastEditedBy = 0,
+                ValidFrom = new DateTime(2016, 5, 31, 23, 11, 0),
+                ValidTo = new DateTime(9999, 12, 31, 23, 59, 59)
+            });
+
+            dbContext.SaveChanges();
+        }
+    }
+}
